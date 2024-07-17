bffdfe0e7a4c552a0634d953d30851998c992eb2
Alex Tellez <alextellezyanes@gmail.com>
2024-07-17 23:06:46 +0200

    fase de JPA and @Entity

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,7 +2,26 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName ="student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
@@ -14,24 +33,24 @@ public class Student {
 
     public Student(Long id,
                     String name,
-                    String email, 
-                    LocalDate dob, 
+                    String email,
+                    LocalDate dob,
                     Integer age) {
         this.id = id;
         this.name = name;
-        this.email = email; 
+        this.email = email;
         this.dob = dob;
-        this.age = age;               
+        this.age = age;
     }
 
     public Student(String name,
-        String email, 
-        LocalDate dob, 
+        String email,
+        LocalDate dob,
         Integer age) {
     this.name = name;
-    this.email = email; 
+    this.email = email;
     this.dob = dob;
-    this.age = age;               
+    this.age = age;
 }
 
     public Long getId(){
@@ -47,7 +66,7 @@ public class Student {
     }
 
     public void setName(String name) {
-        this.name = name; 
+        this.name = name;
     }
 
     public String getEmail() {
@@ -55,7 +74,7 @@ public class Student {
     }
 
     public void setEmail(String email) {
-        this.email = email; 
+        this.email = email;
     }
 
     public LocalDate getDob() {
@@ -67,7 +86,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age; 
+        return age;
     }
 
     public void setAge(Integer age) {
